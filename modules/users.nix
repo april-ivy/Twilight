2a2f7415d28f2291332c6571378444d73c2a8580
April <me@aprl.pet>
2025-11-06 01:41:31 +0000

    update keys

--- modules/users.nix
+++ modules/users.nix
@@ -7,6 +7,7 @@
     packages = with pkgs; [];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINef2ken7G9138zedVAkEyX+gJtZ8SqACmYK3g4D1+0p me@aprl.pet"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEB8ucp3UjKGqBkg2tzX4pUrz50leAUUZoHw9k4ZAzVC nix@aprl.pet"
     ];
   };
 
